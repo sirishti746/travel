c7a60cae356c318391702e8dbf3ae00af033d930
sirishti746 <sirishtisingh@gmail.com>
2024-10-02 00:06:34 +0530

    cb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// client/src/App.js
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Create from "./pages/Create"
+import View from "./pages/View"
+import { useContext } from "react";
+import { AuthContext } from "./authContext";
 
 function App() {
+
+  const { user } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!user) {
+      return <Login/>;
+    } else {
+      return children;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/create" element={<ProtectedRoute><Create/></ProtectedRoute>} />
+        <Route path="/view/:id" element={<ProtectedRoute><View/></ProtectedRoute>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
